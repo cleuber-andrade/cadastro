03b0413313ab09b00df13215f655fc085d60d2d0
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-02-09 15:54:51 -0300

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,22 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Carlos", 22, "3"),
+    new Cliente("Joana", 12, "3"),
+  ]
+
   return (
     <div className={`flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-500 to-purple-500
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteúdo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )
