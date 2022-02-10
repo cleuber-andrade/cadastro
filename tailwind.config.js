98c2104bfcfa8c7879afbbd1ca2196182aa295df
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-02-10 13:02:08 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
-  safelist: [
-    /^bg-/,
-    /^to-/,
-    /^from-/,
-  ],
+  ],  
   theme: {
     extend: {},
   },
