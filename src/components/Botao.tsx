54d56ac9dda847623f6d7c3bda22b811215b55dd
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-02-10 12:47:54 -0300

    update

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -5,9 +5,12 @@ interface BotaoProps{
 }
 
 export default function Botao(props: BotaoProps){
+
+  const cor = props.cor ?? "gray"
+
   return (
     <button className={`
-      bg-gradient-to-r from-blue-400 to-blue-700
+      bg-gradient-to-r from-${cor}-400 to-${cor}-700
       text-white px-4 py-2 rounded-md
       ${props.className} 
     `}>
